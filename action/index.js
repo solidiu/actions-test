329211d294c871dea7ec80b26bacae8f17b09e9f
solidiu <80641794+solidiu@users.noreply.github.com>
2022-09-27 18:11:22 +0200

    Update index.js

--- action/index.js
+++ action/index.js
@@ -1,1 +1,11 @@
-console.log("Ran");
+const fs = require('fs');
+
+const content = Date.now();
+
+fs.writeFile('./output.txt', content, err => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("Successfully wrote " + content + " to the file.");
+  }
+});
