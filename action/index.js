d602c4172dfcb59a532ea3e447a771634a9a558e
solidiu <80641794+solidiu@users.noreply.github.com>
2022-09-27 19:27:22 +0200

    Update index.js

--- action/index.js
+++ action/index.js
@@ -9,13 +9,3 @@ fs.writeFile('./api_cache.txt', content, err => {
     console.log("Successfully wrote " + content + " to the cache file.");
   }
 });
-
-const changed = "true";
-
-fs.writeFile('./changed.txt', changed, err => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log("Successfully wrote " + changed + " to the 'changed' file.");
-  }
-});
